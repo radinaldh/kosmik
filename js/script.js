2e4916ee80c2ccf64f45f75efb37c74efa2efa06
radinaldhs <radinal@wit.id>
2022-04-12 12:07:13 +0700

    fix about link header overlay

--- js/script.js
+++ js/script.js
@@ -1,9 +1,1 @@
-var timeline = document.getElementById("timeline");
-
-function scrollUp() {
-    timeline.scrollBy(0, -50)
-}
-
-function scrollDown() {
-    timeline.scrollBy(0, 50)
-}
+var timeline=document.getElementById("timeline");function scrollUp(){timeline.scrollBy(0,-50)}function scrollDown(){timeline.scrollBy(0,50)}
