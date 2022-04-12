f23db25bd5ea33aa7786bc3afd129f21a06894b6
radinaldhs <radinal@wit.id>
2022-04-12 11:14:31 +0700

    Fix Link Style and scroll button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var timeline = document.getElementById('timeline');
+var timeline = document.getElementById("timeline");
 
 function scrollUp() {
-    timeline.scrollBy(0, -50);
+    timeline.scrollBy(0, -50)
 }
 
 function scrollDown() {
-    timeline.scrollBy(0, 50);
+    timeline.scrollBy(0, 50)
 }
