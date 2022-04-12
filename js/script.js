7cc01fa0102ad082411b6439985e3f1e870f3eac
radinaldhs <radinal@wit.id>
2022-04-13 06:51:02 +0700

    add contact

--- js/script.js
+++ js/script.js
@@ -8,23 +8,11 @@ function scrollDown() {
     timeline.scrollBy(0, 50)
 }
 
-// Animation
-
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-    for (var i = 0; i < reveals.length; i++) {
-        var windowHeight = window.innerHeight;
-        var elementTop = reveals[i].getBoundingClientRect().top;
-        var elementVisible = 150;
-        if (elementTop < windowHeight - elementVisible) {
-            reveals[i].classList.add("active-animation");
-        } else {
-            reveals[i].classList.remove("active-animation");
-        }
+    for (var reveals = document.querySelectorAll(".reveal"), i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight,
+            elementTop, elementVisible = 150;
+        reveals[i].getBoundingClientRect().top < windowHeight - 150 ? reveals[i].classList.add("active-animation") : reveals[i].classList.remove("active-animation")
     }
 }
-
-window.addEventListener("scroll", reveal);
-
-// To check the scroll position on page load
-reveal();
+window.addEventListener("scroll", reveal), reveal();
