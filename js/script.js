93f538080528bc65ab6b35ce183dc3fe288881bb
radinaldhs <radinal@wit.id>
2022-04-12 18:51:31 +0700

    Add animatin fade in

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
-var timeline=document.getElementById("timeline");function scrollUp(){timeline.scrollBy(0,-50)}function scrollDown(){timeline.scrollBy(0,50)}
+var timeline = document.getElementById("timeline");
+
+function scrollUp() {
+    timeline.scrollBy(0, -50)
+}
+
+function scrollDown() {
+    timeline.scrollBy(0, 50)
+}
+
+// Animation
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active-animation");
+        } else {
+            reveals[i].classList.remove("active-animation");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
